72610932137d0d5c94448772ca44025072398f00
armansomoy <armansamoy42@gmail.com>
2021-04-18 21:01:07 +0600

    done

--- js/custom.js
+++ js/custom.js
@@ -10,52 +10,8 @@ $(function () {
         }
     });
 
-    $('.slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        autoplay: true,
-        speed: 1000,
-        arrows: false,
-        fade: true,
-    });
-
-    //service-slide
-    $('.service-slide').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: false,
-        autoplay: true,
-        speed: 1000,
-        arrows: false,
-        responsive: [{
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 3,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                }
-            }
-        ]
-    });
-
-    //counter
-    $('.counter').counterUp({
-        delay: 20,
-        time: 2000
-    });
+   
+    
 
     //video
     $('.venobox').venobox();
